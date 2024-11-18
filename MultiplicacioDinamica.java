5a763e34a8312dc266dbc65821332a34fa53176b
RogerMDev <rogermateom@gmail.com>
2024-11-18 22:14:46 +0100

    Multi

---
+++ MultiplicacioDinamica.java
@@ -0,0 +1,7 @@
+public class MultiplicacioDinamica{
+    public static void main(String[] args) {
+        double Multiplicador = 5.00; 
+        double Multiplicant = 3.00;
+        System.out.print(Multiplicador*Multiplicant);
+    }
+}
